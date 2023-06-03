eafa19e8c3df5839d6f9690299814e171ad4479c
Konstantin Golubkov <konstantingolubkov@MacBook-Air-Konstantin.local>
2023-06-03 11:57:44 +0300

    fixes3.9

--- app/blog/loading.tsx
+++ app/blog/loading.tsx
@@ -1,3 +1,25 @@
+import styles from "./components/Blog.module.scss"
+
 export default function Loading() {
-  return <div>Loading blog...</div>
+  return <div className="placeholder-glow">
+    <div className="breadcrumb">
+      <span className="placeholder col-2"></span>
+      <span className="placeholder col-2"></span>
+      <span className="placeholder col-2"></span>
+    </div>
+    <h1>
+      <span className="placeholder col-3"></span>
+    </h1>
+    
+    <div className={styles.blogItem}>
+      <h2><span className="placeholder col-12"></span></h2>
+      <div className={styles.blogDate}><span className="placeholder col-3"></span></div>
+      <div className={styles.blogShort}>
+        <span className="placeholder col-12"></span>
+        <span className="placeholder col-8"></span>
+        <span className="placeholder col-5"></span>
+      </div>
+    </div>
+
+  </div>
 }
