b66f2169eafb59f500a6aedb3da6fa47f4a979a6
Konstantin Golubkov <konstantingolubkov@MacBook-Air-Konstantin.local>
2023-06-09 20:33:14 +0300

    config3

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  reactStrictMode: true,
-  output: 'export',
-  distDir: '_static',
-  images: {
-    unoptimized: true
-  }
+  // reactStrictMode: true,
+  // output: 'export',
+  // distDir: '_static',
+  // images: {
+  //   unoptimized: true
+  // }
 }
 
 module.exports = nextConfig
