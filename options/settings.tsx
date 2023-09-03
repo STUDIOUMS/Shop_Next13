420628049c932971b45c2b43fc29fe442c79bee7
Konstantin Golubkov <konstantingolubkov@MacBook-Air-Konstantin.local>
2023-09-03 10:08:24 +0300

    update3.1

--- options/settings.tsx
+++ options/settings.tsx
@@ -1,4 +1,4 @@
-export const serverURL: string = process.env.NEXT_PUBLIC_API_URL!
+export const serverURL: string = 'https://qwertynext.com/api/v1'
 export const set_currency: string = 'руб.'
 export const set_phone: string = '+7 (123) 456 78 90'
 export const set_phoneNum: string = '+71234567890'
