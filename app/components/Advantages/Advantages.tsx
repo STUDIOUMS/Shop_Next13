5c859d31438192466596ce5d5e5917e6f4e32989
Kostya <kostya@iMac-Kostya.local>
2023-12-12 14:07:16 +0200

    update 12.12 1.3

--- app/components/Advantages/Advantages.tsx
+++ app/components/Advantages/Advantages.tsx
@@ -16,15 +16,29 @@ const Wrap = styled.div``
 const Item = styled.div`
   border: 1px solid var(--color-light);
   border-radius: var(--radius);
+  color: var(--color-text2);
   padding: var(--gap);
   text-align: center;
-  img {margin-bottom: 20px;}
+  img { margin-bottom: var(--pb); }
+  @media screen and (max-width: 720px) {
+    align-items: center;
+    display: flex;
+    text-align: left;
+    img { margin: 0 20px 0 0; max-width: 40px; }
+  }
 `
 const Title = styled.div`
+  color: var(--color-text);
   font-weight: 600;
   margin: 0 0 10px;
   font-size: 18px;
   line-height: 22px;
+  @media screen and (max-width: 720px) {
+    margin: 0 0 4px;
+  }
+`
+const ItemText = styled.div`
+
 `
 
 const Advantages: React.FC<IAdvantages> = ({ title = 'Преимущества' }) => {
@@ -34,18 +48,24 @@ const Advantages: React.FC<IAdvantages> = ({ title = 'Преимущества' 
       <div className="grid grid-3 grid-mb-1">
         <Item>
           <Image src={truck} alt="" height={50} />
-          <Title>Бесплатная доставка</Title>
-          до вашего склада или терминала ТК
+          <ItemText>
+            <Title>Бесплатная доставка</Title>
+            до вашего склада или терминала ТК
+          </ItemText>
         </Item>
         <Item>
           <Image src={warehouse} alt="" height={50} />
-          <Title>В наличии на складе</Title>
-          весь ассортимент продукции серии &ldquo;Ника&rdquo;
+          <ItemText>
+            <Title>В наличии на складе</Title>
+            весь ассортимент продукции серии &ldquo;Ника&rdquo;
+          </ItemText>
         </Item>
         <Item>
           <Image src={manager} alt="" height={50} />
-          <Title>Выделенный менеджер</Title>
-          Помощь в выборе средств для конкретного предприятия
+          <ItemText>
+            <Title>Выделенный менеджер</Title>
+            Помощь в выборе средств для конкретного предприятия
+          </ItemText>
         </Item>
       </div>
     </Wrap>
