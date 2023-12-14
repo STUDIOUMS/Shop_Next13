d78a479a06cf0d701f99b5927267c255bf71f80f
Kostya <kostya@iMac-Kostya.local>
2023-12-14 11:59:12 +0200

    update 14.12 1.2

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -2,7 +2,7 @@
 
 import { redirect } from 'next/navigation'
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default function PrivateLayout({ children }: { children: React.ReactNode }) {
   const orders = localStorage.getItem('orders')
   if (!orders) redirect('/')
   return (
