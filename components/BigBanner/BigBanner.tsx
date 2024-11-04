df5638550bf1d043def1cd10b71d4ee0771e59cd
K-ost <golubkovru@gmail.com>
2024-11-04 12:53:08 +0200

    bigbanner

--- components/BigBanner/BigBanner.tsx
+++ components/BigBanner/BigBanner.tsx
@@ -1,20 +1,28 @@
-'use client'
+"use client";
 
-import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-import 'swiper/css/autoplay'
-import 'swiper/css/navigation'
-import 'swiper/css/effect-fade'
-import styles from './BigBanner.module.scss'
+import { Swiper, SwiperSlide } from "swiper/react";
+import Section from "@/ui/Section";
+import { Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/navigation";
+import "swiper/css/effect-fade";
+import banner1 from "@/assets/bigbanners/banner1.webp";
+import banner2 from "@/assets/bigbanners/banner2.webp";
+import banner3 from "@/assets/bigbanners/banner3.webp";
+import banner4 from "@/assets/bigbanners/banner4.webp";
+import banner5 from "@/assets/bigbanners/banner5.webp";
+import banner6 from "@/assets/bigbanners/banner6.webp";
+import banner7 from "@/assets/bigbanners/banner7.webp";
+import Banner from "../Banner";
 
-const BigBanner: React.FC = () => {
+const BigBanner = (): JSX.Element => {
   return (
-    <div className="section">
+    <Section>
       <Swiper
         modules={[Autoplay, EffectFade, Navigation, Pagination]}
         autoplay={{
-          delay: 3000
+          delay: 3000,
         }}
         effect="fade"
         spaceBetween={0}
@@ -23,17 +31,30 @@ const BigBanner: React.FC = () => {
         pagination
         loop
       >
-        <SwiperSlide className={`${styles.bigbannerItem} ${styles.bigbannerItem1}`}></SwiperSlide>
-        <SwiperSlide className={`${styles.bigbannerItem} ${styles.bigbannerItem2}`}></SwiperSlide>
-        <SwiperSlide className={`${styles.bigbannerItem} ${styles.bigbannerItem3}`}></SwiperSlide>
-        <SwiperSlide className={`${styles.bigbannerItem} ${styles.bigbannerItem4}`}></SwiperSlide>
-        <SwiperSlide className={`${styles.bigbannerItem} ${styles.bigbannerItem5}`}></SwiperSlide>
-        <SwiperSlide className={`${styles.bigbannerItem} ${styles.bigbannerItem6}`}></SwiperSlide>
-        <SwiperSlide className={`${styles.bigbannerItem} ${styles.bigbannerItem7}`}></SwiperSlide>
-        <SwiperSlide className={`${styles.bigbannerItem} ${styles.bigbannerItem8}`}></SwiperSlide>
+        <SwiperSlide>
+          <Banner src={banner1.src} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Banner src={banner2.src} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Banner src={banner3.src} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Banner src={banner4.src} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Banner src={banner5.src} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Banner src={banner6.src} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Banner src={banner7.src} />
+        </SwiperSlide>
       </Swiper>
-    </div>
-  )
-}
+    </Section>
+  );
+};
 
-export default BigBanner
+export default BigBanner;
