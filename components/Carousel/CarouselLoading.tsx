3b2e9cb91b92048239bda9173d433323c33f4dd4
K-ost <golubkovru@gmail.com>
2024-11-11 22:46:27 +0200

    good loader

--- components/Carousel/CarouselLoading.tsx
+++ components/Carousel/CarouselLoading.tsx
@@ -1,10 +1,22 @@
 import Section from "@/ui/Section";
-import { Skeleton } from "@mui/material";
+import { createArray } from "@/utils/helpers";
+import { Grid2, useMediaQuery, useTheme } from "@mui/material";
+import GoodSkelet from "../Good/GoodSkelet";
 
 const CarouselLoading = (): JSX.Element => {
+  const theme = useTheme();
+  const isTablet = useMediaQuery(theme.breakpoints.down("md"));
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const list = createArray(isMobile ? 1 : isTablet ? 2 : 4);
   return (
     <Section>
-      <Skeleton variant="rounded" height={180} sx={{ mb: 10 }} />
+      <Grid2 container spacing={6}>
+        {list.map((__, index) => (
+          <Grid2 key={index} size={{ xs: 12, sm: 6, md: 3 }}>
+            <GoodSkelet />
+          </Grid2>
+        ))}
+      </Grid2>
     </Section>
   );
 };
